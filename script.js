a6ebf50992f7ceaa0d21e242e9efcca756822899
Sudarshan Suryaprakash <sudarshan@Sudarshans-MacBook-Air.local>
2020-07-24 23:41:50 +0100

    html, css and some js

---
+++ script.js
@@ -0,0 +1,5 @@
+const video = document.getElementById('video');
+const play = document.getElementById('play');
+const stop = document.getElementById('stop');
+const timestamp = document.getElementById('timestamp');
+const progress = document.getElementById('progress');
